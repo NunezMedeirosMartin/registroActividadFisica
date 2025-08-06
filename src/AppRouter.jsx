c73bce864aedb01cb2b76f85fcd0b34d0a0ea49d
nunez <nunezmartin2001@gmail.com>
2025-08-05 21:36:50 -0300

    Fix router basename

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";  
 import "react-toastify/dist/ReactToastify.css";
 
