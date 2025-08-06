f7916e6c88bdf3410f5ed8219b9f1d308ad62465
nunez <nunezmartin2001@gmail.com>
2025-08-05 21:41:08 -0300

    mensaje descriptivo

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,7 +2,6 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";  
 import "react-toastify/dist/ReactToastify.css";
 
-
 import Login from "../src/components/login/login";
 import CargarActividad from "../src/components/Actividad/actividad";
 import VerRegistros from "../src/components/Actividad/registro";
@@ -16,28 +15,28 @@ import GraficoMinutos from "./components/Actividad/graficoMinutos";
 import EvolucionPersonal from "./components/Actividad/evolucionPersonal";
 import Registro from "./components/registro/registrarse";
 
-
 const AppRouter = () => {
   return (
     <>
-    <ToastContainer position="top-right" autoClose={3000} />
-    <Provider store={store}> 
-      <Router basename="/registroActividadFisica">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-          <Route path="/home" element={<App />} />
-          <Route path="/registros" element={<VerRegistros />} />
-          <Route path="/tiempoTotal" element={<TiempoTotal />} />
-          <Route path="/tiempoDiario" element={<TiempoDiario />} />
-          <Route path="/graficoSesiones" element={<GraficoSesiones />} />
-          <Route path="/graficoMinutos" element={<GraficoMinutos />} />
-          <Route path="/evolucion" element={<EvolucionPersonal  />}/>
-        </Routes>
-      </Router>
-    </Provider>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Provider store={store}> 
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/registro" element={<Registro />} />
+            <Route path="/home" element={<App />} />
+            <Route path="/registros" element={<VerRegistros />} />
+            <Route path="/tiempoTotal" element={<TiempoTotal />} />
+            <Route path="/tiempoDiario" element={<TiempoDiario />} />
+            <Route path="/graficoSesiones" element={<GraficoSesiones />} />
+            <Route path="/graficoMinutos" element={<GraficoMinutos />} />
+            <Route path="/evolucion" element={<EvolucionPersonal  />}/>
+          </Routes>
+        </Router>
+      </Provider>
     </>
   );
 };
 
 export default AppRouter;
+
